8a48f6d3ce13b68387f4cd9fdb9071b5ca3bc1bf
uzor samuel ugochukwu <ugosammy98@gmail.com>
2021-05-31 03:18:50 +0100

    Feat: Add search funtiomality ~also added the site logo ~site now resonsive ~user image and bio can now load

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 let button = document.getElementById("search");
-
+const userDetails = document.getElementById("user-details");
 button.addEventListener("click", submit);
 function submit() {
   var query = document.getElementById("username").value;
@@ -7,7 +7,10 @@ function submit() {
   // console.log({e,query});
   // event.preventDefault();
 
-  const token = "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";
+
+ 
+
+  const token = "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";
   fetch("https://api.github.com/graphql", {
     method: "POST",
     headers: {
@@ -19,7 +22,6 @@ function submit() {
       query: `
         query ($queryString: String!) {
           search(query: $queryString, type: USER, first: 1) {
-            repositoryCount
             edges {
               node {
                 ... on User {
@@ -63,11 +65,26 @@ function submit() {
     .then((res) => res.json())
     .then((data) => {
       query = "";
-      // dom elelmt hee
-      // return data.data
+      let result = data.data.search.edges[0].node;
 
-      console.log(data.data);
-      // console.log(data);
+      // console.log(data.data.search.edges[0].node);
+      document.getElementById("user-details").innerHTML = `
+       <div class="details-container">
+       <div>
+       <img class="user-avatar" src=${result.avatarUrl}></img>
+       <h1 class="user-name">${result.name}</h1>
+       <p class="user-bio">${result.bio}</p>
+      </div>
+
+       <div>
+       <div>${result.repositories}</div>
+       </div>
+
+       
+       </div>
+      `;
+    
+console.log(result.repositories);
     })
     .catch((err) => console.log(JSON.stringify(err)));
   //
